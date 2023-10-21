c70e6457cfffb5bf55967acb8c7a1be889f52b2d
Osamu Ishiyama <osamu-ishiyama@two2.jp>
2023-10-21 12:15:45 +0900

    ボタンによるjoke出力、punchline公開

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 // api ref: "https://github.com/15Dkatz/official_joke_api"
-const RANDOM_JOKE_API_URL =
-  "https://official-joke-api.appspot.com/jokes/random";
+const RANDOM_JOKE_API_URL = "https://official-joke-api.appspot.com/jokes/random";
 
 const jokeSetupText = document.getElementById("js-joke-setup");
-const JokeSetupGenerateButton = document.getElementById("js-joke-setup-generate-button");
+const jokeSetupGenerateButton = document.getElementById("js-joke-setup-generate-button");
 const jokePunchlineText = document.getElementById("js-joke-punchline");
-const jokePunchlineGenerateButton = document.getElementById("js-joke-punchline-generate-button");
+const jokePunchlineOpenButton = document.getElementById("js-joke-punchline-open-button");
 
-JokeSetupGenerateButton.addEventListener("click", function () {});
+jokeSetupGenerateButton.addEventListener("click", function () {
+  renderJokeSetup();
+  jokePunchlineOpenButton.style.display = "block";
+});
+
+jokePunchlineOpenButton.addEventListener("click", function () {
+  jokePunchlineText.style.display = "block";
+})
 
 /* fetch joke text */
 async function fetchJokeText() {
