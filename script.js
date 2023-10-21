d36a45228e7d2e4a75f0216ca2e9c978d159af38
Osamu Ishiyama <osamu-ishiyama@two2.jp>
2023-10-21 16:50:19 +0900

    reset処理の追加

--- script.js
+++ script.js
@@ -1,31 +1,48 @@
-// api ref: "https://github.com/15Dkatz/official_joke_api"
+// API ref: "https://github.com/15Dkatz/official_joke_api"
 const RANDOM_JOKE_API_URL = "https://official-joke-api.appspot.com/jokes/random";
 
 const jokeSetupText = document.getElementById("js-joke-setup");
 const jokeSetupGenerateButton = document.getElementById("js-joke-setup-generate-button");
 const jokePunchlineText = document.getElementById("js-joke-punchline");
 const jokePunchlineOpenButton = document.getElementById("js-joke-punchline-open-button");
+const resetButton = document.getElementById("js-reset-button");
 
+/* Generate joke button */
 jokeSetupGenerateButton.addEventListener("click", function () {
+  jokeSetupText.textContent = "generating....";
   jokeSetupText.style.visibility = "visible";
+
   renderJokeSetup();
+
   jokePunchlineOpenButton.classList.remove("unable-button");
   jokeSetupGenerateButton.classList.add("unable-button");
 });
 
+/* Open joke punchline button */
 jokePunchlineOpenButton.addEventListener("click", function () {
   jokePunchlineText.style.visibility = "visible";
+
   jokeSetupGenerateButton.classList.add("unable-button");
   jokePunchlineOpenButton.classList.add("unable-button");
+  resetButton.classList.remove("unable-button");
 })
 
-/* fetch joke text */
+resetButton.addEventListener("click", function () {
+  jokeSetupText.textContent = "";
+  jokePunchlineText.textContent = "";
+  jokePunchlineText.style.visibility = "hidden";
+
+  resetButton.classList.add("unable-button");
+  jokeSetupGenerateButton.classList.remove("unable-button");
+})
+
+/* Fetch joke text */
 async function fetchJokeText() {
   const response = await fetch(RANDOM_JOKE_API_URL);
   return await response.json();
 }
 
-/* render joke setup and set punchline */
+/* Render joke setup and set punchline */
 async function renderJokeSetup() {
   const joke = await fetchJokeText();
   jokeSetupText.textContent = joke.setup;
@@ -34,7 +51,7 @@ async function renderJokeSetup() {
   setJokePunchline(joke.punchline);
 }
 
-/* set joke punchline */
+/* Set joke punchline */
 function setJokePunchline(jokePunchline) {
   jokePunchlineText.textContent = jokePunchline;
 }
