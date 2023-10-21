4a6d5b485d5fc9967d91bfb95c7fe1d02b7a362d
Osamu Ishiyama <osamu-ishiyama@two2.jp>
2023-10-21 12:01:41 +0900

    リファクタ

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 // api ref: "https://github.com/15Dkatz/official_joke_api"
-const RANDOM_JOKE_API_URL = "https://official-joke-api.appspot.com/jokes/random"
+const RANDOM_JOKE_API_URL =
+  "https://official-joke-api.appspot.com/jokes/random";
 
 const jokeSetupText = document.getElementById("js-joke-setup");
 const JokeSetupGenerateButton = document.getElementById("js-joke-setup-generate-button");
-const jokePunchlineText = document.getElementById("js-joke-punchline")
+const jokePunchlineText = document.getElementById("js-joke-punchline");
 const jokePunchlineGenerateButton = document.getElementById("js-joke-punchline-generate-button");
 
-JokeSetupGenerateButton.addEventListener("click", function () {
-});
+JokeSetupGenerateButton.addEventListener("click", function () {});
 
 /* fetch joke text */
 async function fetchJokeText() {
-  const response = await fetch(RANDOM_JOKE_API_URL)
+  const response = await fetch(RANDOM_JOKE_API_URL);
   return await response.json();
 }
 
@@ -26,5 +26,5 @@ async function renderJokeSetup() {
 
 /* set joke punchline */
 function setJokePunchline(jokePunchline) {
-  jokePunchlineText.textContent = jokePunchline
-} 
+  jokePunchlineText.textContent = jokePunchline;
+}
